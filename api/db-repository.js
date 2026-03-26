16af3a2ff7efab151d46fd5eb54fdf9395584083
Matěj Slíva <SlivaMatej@litv.sssvt.cz>
2026-03-26 09:39:10 +0100

    Initial setup

---
+++ api/db-repository.js
@@ -0,0 +1,10 @@
+import mysql from 'mysql'
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'mysqlstudenti.litv.sssvt.cz',
+    user: 'slivamatej',
+    password: '123456',
+    database: ''
+});
---
+++ api/index.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import cors from 'cors'
+
+const PORT = 4200;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
