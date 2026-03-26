20c0721776949c3136a374c49a179de4980790a5
sleewga <matej.plum7@proton.me>
2026-03-26 20:15:43 +0100

    Initial css

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 
 const PORT = 4200;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.listen(PORT, () => {});
+
