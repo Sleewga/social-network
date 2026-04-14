925b5d50e3b18d9855a4086a9c534fb4e3a54364
sleewga <matej.plum7@proton.me>
2026-04-14 21:55:21 +0200

    Redesign post, add page profile

--- web/post-manager.js
+++ web/post-manager.js
@@ -6,43 +6,58 @@ export class PostManager{
     }
 
     generateArticles(articles){
-        // console.log(articles);
-        // console.log(articles[0]);
-        for (let i = 0; i < articles.length; i++){
-            let articleElement = this.getArticleElement(articles[i]);
+        const articleElementsFragment = this.getArticleElementsFragment(articles);
+        this.#articlesElement.appendChild(articleElementsFragment);
+    }
 
-            this.#articlesElement.appendChild(articleElement);
+    getArticleElementsFragment(articles){
+        const articleElementsFragment = document.createDocumentFragment();
+
+        for (let i = 0; i < articles.length; i++){
+            const articleElement = this.getArticleElement(articles[i]);
+            articleElementsFragment.appendChild(articleElement);
         }
+
+        return articleElementsFragment;
     }
 
     getArticleElement(article){
+        const content = document.createElement("p");
+
         const articleElement = document.createElement("li")
         articleElement.classList.add("article", "container");
 
+        const articleHeaderElement = this.getArticleHeaderElement(article);
 
+        content.innerText = article.content;
+
+        articleElement.appendChild(articleHeaderElement);
+        articleElement.appendChild(content);
+
+        return articleElement;
+    }
+
+    getArticleHeaderElement(article){
         const title = document.createElement("h2");
+        const author = document.createElement("a");
+        const date = document.createElement("span");
+
+        const articleHeader = document.createElement("section");
+        articleHeader.classList.add("article-header");
 
         const articleQuickInfo = document.createElement("div")
         articleQuickInfo.classList.add("article-quick-info");
 
-        const author = document.createElement("a");
-        const date = document.createElement("span");
-
+        title.innerText = article.title;
         author.innerText = article.username;
         let dateFormat = new Date(article.creation_time);
-        date.innerText = dateFormat;
+        date.innerText = dateFormat.toLocaleDateString();
+
+        articleHeader.appendChild(title);
+        articleHeader.appendChild(articleQuickInfo);
         articleQuickInfo.appendChild(author);
         articleQuickInfo.appendChild(date);
 
-        const content = document.createElement("p");
-
-        title.innerText = article.title;
-        content.innerText = article.content;
-
-        articleElement.appendChild(title);
-        articleElement.appendChild(articleQuickInfo);
-        articleElement.appendChild(content);
-
-        return articleElement;
+        return articleHeader;
     }
 }
