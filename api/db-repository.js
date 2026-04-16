86bebcd7acde16ee4df431ce8c482b67b7157f5c
sleewga <matej.plum7@proton.me>
2026-04-16 15:26:07 +0200

    Add .env solution

--- api/db-repository.js
+++ api/db-repository.js
@@ -1,10 +1,11 @@
+import "dotenv/config";
 import mysql from "mysql";
 
 const connection = mysql.createConnection({
-    host: "mysqlstudenti.litv.sssvt.cz",
-    user: "slivamatej",
-    password: "123456",
-    database: "4a2_slivamatej_db1"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
